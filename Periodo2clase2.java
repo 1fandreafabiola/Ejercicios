3d65d5c414e2b51fa2b50a7cfeffde3fd88e5840
1fandreafabiola <112784005+1fandreafabiola@users.noreply.github.com>
2022-09-11 16:26:29 -0600

    Add files via upload

---
+++ Periodo2clase2.java
@@ -0,0 +1,10 @@
+package com.mycompany.periodo2clase2;
+public class Periodo2clase2 {
+    public static void main(String[] args) {
+         String cadena= "";
+      for (int i=1; i<=20; i++){
+          cadena = cadena + "*";
+          System.out.println(cadena);
+      }
+    }
+}
